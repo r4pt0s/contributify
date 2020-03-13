3145f7a4d3a956a7e599694ba43df9d1a913cdab
Wolfgang Kreminger <r4pt0s@protonmail.com>
2020-03-13 21:31:55 +0100

    changed to commit and push approach via octokit

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run(payload) {
     this.requested_reviewers = payload.pull_request.requested_reviewers;
     this.checks = {};
  */
+
   console.log(payload.pull_request.user);
 
   main(payload.pull_request.user);
@@ -41,7 +42,7 @@ async function main(userLogin) {
   const patterns = ["**/CONTRIBUTORS.md"];
   const globber = await glob.create(patterns.join("\n"));
   const files = await globber.glob();
-  let isUserInFile = { fileExists: false };
+  let isUserInFile = { userExists: false };
 
   console.log(files);
   if (files.length > 0) {
@@ -53,17 +54,43 @@ async function main(userLogin) {
       "========================================================================"
     );
     console.log("IS USER IN FILE: ", isUserInFile);
-  }
 
-  if (!isUserInFile.fileExists) {
-    await createAndCommitFile(
+    if (!isUserInFile.userExists) {
+      await setRepo(github.context.repo.owner, github.context.repo.repo);
+      await setBranch("master");
+      await pushFiles("Making a commit with my adorable files", [
+        { content: "You are a Wizard, Harry", path: "harry.txt" },
+        { content: "May the Force be with you", path: "jedi.txt" }
+      ]);
+      console.log("Files committed!");
+
+      /*  await createAndCommitFile(
       userLogin.login,
       userLogin.html_url,
       isUserInFile.sha
-    );
+    ); */
+    } else {
+      console.log("=================================");
+      console.log("USER IS ALREADY IN FILE....");
+    }
   } else {
-    console.log("=================================");
-    console.log("USER IS ALREADY IN FILE....");
+    // IF the file doesn't exist, create the file
+    try {
+      await setRepo(github.context.repo.owner, github.context.repo.repo);
+      await setBranch("master");
+      await pushFiles(
+        `CONTRIBUTIFY BOT added ${userLogin.login} to CONTRIBUTORS.md file`,
+        [
+          {
+            content: `\n- [@${userLogin.login}](${userLogin.html_url})`,
+            path: "CONTRIBUTORS.md"
+          }
+        ]
+      );
+      console.log("Files committed!");
+    } catch (err) {
+      console.log("ERROR: ", err);
+    }
   }
 }
 //!!!!!!!
@@ -78,7 +105,7 @@ async function checkIfContributorExists(loginName) {
   const fileContents = Buffer.from(result.data.content, "base64").toString();
 
   console.log("login name:", loginName);
-  return { fileExists: fileContents.includes(loginName), sha: result.data.sha };
+  return { userExists: fileContents.includes(loginName), sha: result.data.sha };
 }
 //!!!!!!!!!
 
@@ -89,17 +116,6 @@ async function createAndCommitFile(loginName, profileUrl, fileSha) {
   const payload = github.context.payload;
 
   try {
-    const currCommit = await getCurrentCommit();
-
-    console.log("new commit: ", newCommit);
-    const newCommit = await createNewCommit(
-      `CONTRIBUTIFY BOT added ${loginName} to CONTRIBUTORS.md file`,
-      currCommit.treeSha,
-      currCommit.commitSha
-    );
-
-    await setBranchToCommit("master", newCommit.sha);
-
     // commit message => `CONTRIBUTIFY BOT added ${loginName} to CONTRIBUTORS.md file`
     await octokit.repos.createOrUpdateFile({
       owner: github.context.repo.owner,
@@ -109,7 +125,7 @@ async function createAndCommitFile(loginName, profileUrl, fileSha) {
         "base64"
       ),
       path: `${filename}`,
-      sha: newCommit.sha, //github.context.payload.pull_request.base.sha,
+      sha: fileSha, //github.context.payload.pull_request.base.sha,
       branch: "master"
     });
   } catch (err) {
@@ -118,6 +134,90 @@ async function createAndCommitFile(loginName, profileUrl, fileSha) {
 
   console.log("=================================");
   console.log("GENERATED FILE AND PUSHED IT TO MASTER RIGHT NOW");
+}
+
+let repo;
+let filesToCommit = [];
+let currentBranch = {};
+let newCommit = {};
+
+const setRepo = function(userName, repoName) {
+  repo = octokit.getRepo(userName, repoName);
+};
+
+const setBranch = function(branchName) {
+  return repo.listBranches().then(branches => {
+    let branchExists = branches.data.find(branch => branch.name === branchName);
+    if (!branchExists) {
+      return repo.createBranch("master", branchName).then(() => {
+        currentBranch.name = branchName;
+      });
+    } else {
+      currentBranch.name = branchName;
+    }
+  });
+};
+
+const pushFiles = function(message, files) {
+  return getCurrentCommitSHA()
+    .then(getCurrentTreeSHA)
+    .then(() => createFiles(files))
+    .then(createTree)
+    .then(() => createCommit(message))
+    .then(updateHead)
+    .catch(e => {
+      console.error(e);
+    });
+};
+
+function getCurrentCommitSHA() {
+  return repo.getRef("heads/" + currentBranch.name).then(ref => {
+    currentBranch.commitSHA = ref.data.object.sha;
+  });
+}
+
+function getCurrentTreeSHA() {
+  return repo.getCommit(currentBranch.commitSHA).then(commit => {
+    currentBranch.treeSHA = commit.data.tree.sha;
+  });
+}
+
+function createFiles(files) {
+  let promises = [];
+  let length = filesInfo.length;
+  for (let i = 0; i < length; i++) {
+    promises.push(createFile(files[i]));
+  }
+  return Promise.all(promises);
+}
+
+function createFile(file) {
+  return repo.createBlob(file.content).then(blob => {
+    filesToCommit.push({
+      sha: blob.data.sha,
+      path: fileInfo.path,
+      mode: "100644",
+      type: "blob"
+    });
+  });
+}
+
+function createTree() {
+  return repo.createTree(filesToCommit, currentBranch.treeSHA).then(tree => {
+    newCommit.treeSHA = tree.data.sha;
+  });
+}
+
+function createCommit(message) {
+  return repo
+    .commit(currentBranch.commitSHA, newCommit.treeSHA, message)
+    .then(commit => {
+      newCommit.sha = commit.data.sha;
+    });
+}
+
+function updateHead() {
+  return repo.updateHead("heads/" + currentBranch.name, newCommit.sha);
 }
 
 //! DONE !!!
